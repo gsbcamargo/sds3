427e673c9cc9562cf2f6a4c00df96a0b55dcd834
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-05-07 01:26:22 -0300

    project created

---
+++ backend/src/main/java/com/gabriel/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
